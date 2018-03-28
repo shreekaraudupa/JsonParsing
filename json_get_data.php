ca2cddb0cfe64f1d9a7f02c5d07477776d46fcc1
shreekara  udupa <shreekaraudupa@gmail.com>
2018-03-28 09:49:48 +0530

    Add files via upload

---
+++ json_get_data.php
@@ -0,0 +1,25 @@
+<?php
+
+$host  = "localhost";
+$user_name=  "root";
+$password  = "";
+$db_name = "project";
+$filter= "4000000"
+
+$sql=  "select * from project_info ;";
+
+$con = mysqli_connect($host,$user_name,$password,$db_name);
+
+$result = mysqli_query( $con, $sql);
+
+$response = array();
+while( $row =  mysqli_fetch_array( $result ))     
+{
+	array_push($response, array("name"=>$row[0],"details"=>$row[1],"project_id"=>$row[2]));
+}
+
+echo json_encode(array("  "=>$response));
+
+mysqli_close($con);
+
+?>
